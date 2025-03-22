0360b8f84e13558ccb4d9d32eb15be566f342b4b
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2025-03-22 23:04:34 +0530

    correct files/folder push

---
+++ byteforge-frontend/src/pages/About.page.tsx
@@ -0,0 +1,12 @@
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+
+export function AboutPage() {
+  return (
+    <>
+      <div>
+        <h1>About Us</h1>
+      </div>
+      <ColorSchemeToggle />
+    </>
+  );
+}
