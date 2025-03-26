1bb6f76c3af69aaf227ce012f7c30fb4326451bc
MehulChauhan-07 <mehulsinhchauhan0512@gmail.com>
2025-03-27 05:04:02 +0530

    Auto stash before cherry pick of "try to protect fearture from unauthorized user"

--- byteforge-frontend/src/main.tsx
+++ byteforge-frontend/src/main.tsx
@@ -4,17 +4,26 @@ import { BrowserRouter } from "react-router-dom";
 import App from "@/App";
 import { ThemeProvider } from "@/components/common/ThemeProvider";
 import "@/index.css";
+import { ProgressProvider } from "./context/ProgressContex";
 
 import { ProgressProvider } from "./contexts/ProgressContex";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+<<<<<<< Updated upstream
       <ProgressProvider>
         <ThemeProvider defaultTheme="system" storageKey="byteforge-theme">
           <App />
         </ThemeProvider>
       </ProgressProvider>
+=======
+      <ThemeProvider defaultTheme="system" storageKey="byteforge-theme">
+        <ProgressProvider>
+          <App />
+        </ProgressProvider>
+      </ThemeProvider>
+>>>>>>> Stashed changes
     </BrowserRouter>
   </React.StrictMode>
 );
