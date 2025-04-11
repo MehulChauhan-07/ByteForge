439ce3b033f2a0a6b256439a6cfc9a8832a16cd7
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2025-04-12 02:50:12 +0530

    add the shadCN ui

--- byteforge-frontend/vite.config.ts
+++ byteforge-frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
+import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -14,7 +14,7 @@ export default defineConfig({
       port: 5173, // Explicitly match your dev server port
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
